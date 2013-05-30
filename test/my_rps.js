31c121737e7bd483f0c4a87c8d6aabbfb19bb412
Robert Jackiewicz <rob@jackiewicz.ca>
2013-05-30 14:22:37 -0400

    Refactored the tests.

--- test/my_rps.js
+++ test/my_rps.js
@@ -4,54 +4,48 @@ var expect = chai.expect;
 var RPS = global.RPS;
 
 describe("RobsPlayer", function() {
+  beforeEach(function(){ myPlayer = new RPS.Player.RobsPlayer(); });
+
   describe("Constructor", function() {
     it('should set its name properly', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       expect(myPlayer.NAME).to.equal("Rob's Player");
     });
   });
 
   describe(".throwMove()", function() {
     it('1st move should be SCISSORS', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       expect(myPlayer.throwMove()).to.equal(RPS.Moves.SCISSORS);
     });
 
     it('should throw PAPER if the last two moves were SCISSORS', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       myPlayer.addResult(0, RPS.Moves.SCISSORS);
       myPlayer.addResult(0, RPS.Moves.SCISSORS);
       expect(myPlayer.throwMove()).to.equal(RPS.Moves.PAPER);
     });
 
     it('should throw SCISSORS if the last two moves were ROCK', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       myPlayer.addResult(0, RPS.Moves.ROCK);
       myPlayer.addResult(0, RPS.Moves.ROCK);
       expect(myPlayer.throwMove()).to.equal(RPS.Moves.SCISSORS);
     });
 
     it('should throw ROCK if the last two moves were PAPER', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       myPlayer.addResult(0, RPS.Moves.PAPER);
       myPlayer.addResult(0, RPS.Moves.PAPER);
       expect(myPlayer.throwMove()).to.equal(RPS.Moves.ROCK);
     });
 
     it('should throw ROCK if the opponent lost last with PAPER', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       myPlayer.addResult(1, RPS.Moves.PAPER);
       expect(myPlayer.throwMove()).to.equal(RPS.Moves.ROCK);
     });
 
     it('should throw SCISSORS if the opponent lost last with ROCK', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       myPlayer.addResult(1, RPS.Moves.ROCK);
       expect(myPlayer.throwMove()).to.equal(RPS.Moves.SCISSORS);
     });
 
     it('should throw PAPER if the opponent lost last with SCISSORS', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       myPlayer.addResult(1, RPS.Moves.SCISSORS);
       expect(myPlayer.throwMove()).to.equal(RPS.Moves.PAPER);
     });
@@ -59,7 +53,6 @@ describe("RobsPlayer", function() {
 
   describe(".addResult(result, opponentMove)", function(){
     it('should only set the last two moves of the opponent', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       myPlayer.addResult(0, RPS.Moves.SCISSORS);
       myPlayer.addResult(1, RPS.Moves.PAPER);
       myPlayer.addResult(0, RPS.Moves.ROCK);
@@ -67,7 +60,6 @@ describe("RobsPlayer", function() {
     });
 
     it('should record whether it won or lost the last round', function() {
-      myPlayer = new RPS.Player.RobsPlayer();
       myPlayer.addResult(1, RPS.Moves.ROCK);
       expect(myPlayer.won).to.equal(1);
       myPlayer.addResult(0, RPS.Moves.ROCK);
